f61fbb1a233744bf3b66cf506ec8b201d600a6c4
PierreLouis <plmariepro@gmail.com>
2023-05-29 12:04:16 +0200

    erreur 404 sur id

--- kasa/src/pages/accomodation/Accomodation.js
+++ kasa/src/pages/accomodation/Accomodation.js
@@ -1,6 +1,7 @@
 import './accomodation.scss'
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom";
+import NotFound from '../notFound/NotFound';
 import datas from '../../data/data'
 import Header from "../../components/header/Header";
 import Slider from "../../components/carousel/Carousel"
@@ -12,66 +13,81 @@ import redStar from '../../assets/red_star.png';
 export default function Accomodation() {
 
     const [imageSlider, setImageSlider] = useState([]);
+    const [error, setError] = useState(false);
+    const [house, setHouse] = useState([]);
 
     const idAccomodation = useParams('id').id;
-    const dataCurrentAccomodation = datas.filter(data => data.id === idAccomodation);
+
+    function filterHouse() {
+        const house = datas.filter(data => data.id === idAccomodation);
+        if (house.length === 0) setError(true)
+        return house;
+    }
 
     useEffect(() => {
         const dataCurrentAccomodation = datas.filter(data => data.id === idAccomodation);
+        setHouse(filterHouse());
+        if (house.length === 0) {
+            setError(true);
+            console.log('retertetr')
+        }
         setImageSlider(dataCurrentAccomodation[0].pictures);
     }, [idAccomodation]);
-
-    const name = dataCurrentAccomodation[0].host.name.split(' ');
-    const rating = dataCurrentAccomodation[0].rating;
-    const description = dataCurrentAccomodation[0].description;
-    const equipments = dataCurrentAccomodation[0].equipments;
-
+    console.log(error)
+    console.log(house)
     return (
         <>
-            <Header />
-            <Slider imageSlider={imageSlider} />
-            <main className="accomodation">
-                <div className="accomodation_content">
-                    <div className="accomodation_content_infos">
-                        <h1>{dataCurrentAccomodation[0].title}</h1>
-                        <p>{dataCurrentAccomodation[0].location}</p>
-                        <div>
-                            {dataCurrentAccomodation[0].tags.map((tag, index) => {
-                                return (
-                                    <button key={index}>{tag}</button>
-                                )
-                            })}
-                        </div>
-                    </div>
-                    <div className="accomodation_content_host">
-                        <div>
-                            <div className='accomodation_content_host_name'>
-                                <span>{name[0]}</span>
-                                <span>{name[1]}</span>
-                            </div>
-                            <img src={dataCurrentAccomodation[0].host.picture} alt="host of this accomodation" />
-                        </div>
+            {
+                error === true ? <NotFound /> :
+                    house ?
+                        <>
+                            <Header />
+                            <Slider imageSlider={imageSlider} />
+                            <main className="accomodation">
+                                <div className="accomodation_content">
+                                    <div className="accomodation_content_infos">
+                                        <h1>{house[0].title}</h1>
+                                        <p>{house[0].location}</p>
+                                        <div>
+                                            {house[0].tags.map((tag, index) => {
+                                                return (
+                                                    <button key={index}>{tag}</button>
+                                                )
+                                            })}
+                                        </div>
+                                    </div>
+                                    <div className="accomodation_content_host">
+                                        <div>
+                                            <div className='accomodation_content_host_name'>
+                                                <span>{house[0].host.name.split(' ')[0]}</span>
+                                                <span>{house[0].host.name.split(' ')[1]}</span>
+                                            </div>
+                                            <img src={house[0].host.picture} alt="host of this accomodation" />
+                                        </div>
 
-                        <div className="accomodation_content_host_stars">
-                            {[...Array(5)].map((star, index) => {
-                                const ratingValue = index + 1;
-                                return (
-                                    <img key={index} src={ratingValue <= rating ? redStar : greyStar} alt="star" />
-                                )
-                            })}
-                        </div>
-                    </div>
-                </div>
-                <div className="accomodation_collapse">
-                    <div className="accomodation_collapse_item">
-                        <Collapse title={'Description'} content={description} />
-                    </div>
-                    <div className="accomodation_collapse_item">
-                        <Collapse title={'Équipements'} content={equipments} />
-                    </div>
-                </div>
-            </main>
-            <Footer />
+                                        <div className="accomodation_content_host_stars">
+                                            {[...Array(5)].map((star, index) => {
+                                                const ratingValue = index + 1;
+                                                return (
+                                                    <img key={index} src={ratingValue <= house[0].rating ? redStar : greyStar} alt="star" />
+                                                )
+                                            })}
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="accomodation_collapse">
+                                    <div className="accomodation_collapse_item">
+                                        <Collapse title={'Description'} content={house[0].description} />
+                                    </div>
+                                    <div className="accomodation_collapse_item">
+                                        <Collapse title={'Équipements'} content={house[0].equipments} />
+                                    </div>
+                                </div>
+                            </main>
+                            <Footer />
+                        </>
+                        : <p>erzlmkjertlk</p>
+            }
         </>
     )
 }
