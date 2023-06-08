66836201abb481a7265730fc3b8b766962c42167
PierreLouis <plmariepro@gmail.com>
2023-06-08 11:22:59 +0200

    pb footer

--- kasa/src/pages/accomodation/Accomodation.js
+++ kasa/src/pages/accomodation/Accomodation.js
@@ -1,8 +1,7 @@
 import './accomodation.scss'
 import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom";
-import NotFound from '../notFound/NotFound';
-import datas from '../../data/data'
+import { useParams, useNavigate } from "react-router-dom";
+import datas from '../../data/data.json'
 import Header from "../../components/header/Header";
 import Slider from "../../components/carousel/Carousel"
 import Footer from "../../components/footer/Footer";
@@ -11,83 +10,73 @@ import greyStar from '../../assets/grey_star.png';
 import redStar from '../../assets/red_star.png';
 
 export default function Accomodation() {
-
     const [imageSlider, setImageSlider] = useState([]);
-    const [error, setError] = useState(false);
-    const [house, setHouse] = useState([]);
+    const [loader, setLoader] = useState(true);
 
+    const navigate = useNavigate();
     const idAccomodation = useParams('id').id;
-
-    function filterHouse() {
-        const house = datas.filter(data => data.id === idAccomodation);
-        if (house.length === 0) setError(true)
-        return house;
-    }
+    const dataCurrentAccomodation = datas.filter(data => data.id === idAccomodation);
 
     useEffect(() => {
-        const dataCurrentAccomodation = datas.filter(data => data.id === idAccomodation);
-        setHouse(filterHouse());
-        if (house.length === 0) {
-            setError(true);
-            console.log('retertetr')
+        try {
+            const dataCurrentAccomodation = datas.filter(data => data.id === idAccomodation);
+            setImageSlider(dataCurrentAccomodation[0].pictures);
+            setLoader(false);
+        } catch {
+            navigate('*')
         }
-        setImageSlider(dataCurrentAccomodation[0].pictures);
-    }, [idAccomodation]);
-    console.log(error)
-    console.log(house)
+    },
+    );
+
     return (
         <>
+            <Header />
+            <Slider imageSlider={imageSlider} />
             {
-                error === true ? <NotFound /> :
-                    house ?
-                        <>
-                            <Header />
-                            <Slider imageSlider={imageSlider} />
-                            <main className="accomodation">
-                                <div className="accomodation_content">
-                                    <div className="accomodation_content_infos">
-                                        <h1>{house[0].title}</h1>
-                                        <p>{house[0].location}</p>
-                                        <div>
-                                            {house[0].tags.map((tag, index) => {
-                                                return (
-                                                    <button key={index}>{tag}</button>
-                                                )
-                                            })}
-                                        </div>
+                loader ? <p>Chargement</p> :
+                    <main className="accomodation">
+                        <div className="accomodation_content">
+                            <div className="accomodation_content_infos">
+                                <h1>{dataCurrentAccomodation[0].title}</h1>
+                                <p>{dataCurrentAccomodation[0].location}</p>
+                                <div>
+                                    {dataCurrentAccomodation[0].tags.map((tag, index) => {
+                                        return (
+                                            <button key={index}>{tag}</button>
+                                        )
+                                    })}
+                                </div>
+                            </div>
+                            <div className="accomodation_content_host">
+                                <div>
+                                    <div className='accomodation_content_host_name'>
+                                        <span>{dataCurrentAccomodation[0].host.name.split(' ')[0]}</span>
+                                        <span>{dataCurrentAccomodation[0].host.name.split(' ')[1]}</span>
                                     </div>
-                                    <div className="accomodation_content_host">
-                                        <div>
-                                            <div className='accomodation_content_host_name'>
-                                                <span>{house[0].host.name.split(' ')[0]}</span>
-                                                <span>{house[0].host.name.split(' ')[1]}</span>
-                                            </div>
-                                            <img src={house[0].host.picture} alt="host of this accomodation" />
-                                        </div>
+                                    <img src={dataCurrentAccomodation[0].host.picture} alt="host of this accomodation" />
+                                </div>
 
-                                        <div className="accomodation_content_host_stars">
-                                            {[...Array(5)].map((star, index) => {
-                                                const ratingValue = index + 1;
-                                                return (
-                                                    <img key={index} src={ratingValue <= house[0].rating ? redStar : greyStar} alt="star" />
-                                                )
-                                            })}
-                                        </div>
-                                    </div>
+                                <div className="accomodation_content_host_stars">
+                                    {[...Array(5)].map((star, index) => {
+                                        const ratingValue = index + 1;
+                                        return (
+                                            <img key={index} src={ratingValue <= dataCurrentAccomodation[0].rating ? redStar : greyStar} alt="star" />
+                                        )
+                                    })}
                                 </div>
-                                <div className="accomodation_collapse">
-                                    <div className="accomodation_collapse_item">
-                                        <Collapse title={'Description'} content={house[0].description} />
-                                    </div>
-                                    <div className="accomodation_collapse_item">
-                                        <Collapse title={'Équipements'} content={house[0].equipments} />
-                                    </div>
-                                </div>
-                            </main>
-                            <Footer />
-                        </>
-                        : <p>erzlmkjertlk</p>
+                            </div>
+                        </div>
+                        <div className="accomodation_collapse">
+                            <div className="accomodation_collapse_item">
+                                <Collapse title={'Description'} content={dataCurrentAccomodation[0].description} />
+                            </div>
+                            <div className="accomodation_collapse_item">
+                                <Collapse title={'Équipements'} content={dataCurrentAccomodation[0].equipments} />
+                            </div>
+                        </div>
+                    </main>
             }
+            <Footer />
         </>
     )
 }
